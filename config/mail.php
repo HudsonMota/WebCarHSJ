8a0376f6a915b6422cf2b73f0e775267b08e99a9
Hudson Rodrigues <hudson.rodrigues@hsj.intranet>
2024-04-08 19:39:35 -0300

    alterados os emails para inserção do usuário no arquivo .env e no arquivo config\mail.php

--- config/mail.php
+++ config/mail.php
@@ -83,8 +83,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'developmentsoftware.hsj@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'developmentsoftware.hsj@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'EMAIL'),
+        'name' => env('MAIL_FROM_NAME', 'EMAIL'),
     ],
 
     /*
